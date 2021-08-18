24d61f922acb655d831cedd0f3daaae818003a80
Aleksandr Kuzmichev <d5f@ya.ru>
2021-08-18 20:16:36 +0300

    Added args parameter

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String[] args){
-		String greeting = "Hello world!";
+		String greeting = "Hello world "+args[0];
 		System.out.println(greeting);
 	}
 }
