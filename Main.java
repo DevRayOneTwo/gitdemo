5ac3eaf2510dae01e3ef946b887f987e384872c3
Aleksandr Kuzmichev <d5f@ya.ru>
2021-08-18 20:57:09 +0300

    added Main class documentation

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
 	public static void main(String[] args){
+
+		//a variable that stores string value
 		String greeting = "Hello world "+args[0];
 		System.out.println(greeting);
 	}
